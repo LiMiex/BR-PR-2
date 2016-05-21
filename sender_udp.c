bcd3341ef51458d9d6237ace94e1195baa68786a
LiMiex <ashek@uos.de>
2016-05-21 19:40:08 +0200

    added fopen to sender_udp implemented file length

--- sender_udp.c
+++ sender_udp.c
@@ -12,22 +12,38 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include "file.c"
 
 
 
 
-int main (int argc, char **argv) {
+int main (int argc, char *argv[]) {
     char buffer[256];
     struct sockaddr_in server;
-    int sock;
+    int sock, send;
+    long fileSize;
     
-    //creating socket
+    FILE *fp = fopen(argv[3], "a");
+    if (fp == NULL) {
+        puts("error in file handling");
+        return 1;
+    }
+    
+    fileSize = fileLength(fp);
+    
+    if (argc != 4){
+        printf("arguments invalid");
+    //creating udp socket
     if((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         printf("cannot create socket");
         return 0;
     }
-    //serverstruktur
+    //server structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl(argv[1]);
     server.sin_port = htons(atoi(argv[2]));
+    
+    if((sendto(sock, )) < 0){
+        printf("cannot sendto server");
+    }
 }
