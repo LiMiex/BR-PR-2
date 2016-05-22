ada1def5106e9e328614160b354d573c71b92a05
LiMiex <ashek@uos.de>
2016-05-23 00:34:41 +0200

    updated sendto

--- sender_udp.c
+++ sender_udp.c
@@ -28,7 +28,7 @@ int main (int argc, char *argv[]) {
     //bzero(buffer,BUFFERSIZE);
     struct sockaddr_in dest, from;
     socklen_t addr_size, client_addr_size;
-    
+    int counter;
 
     int sock, send, recv, clo, flen;
     unsigned short filenameLength;
@@ -74,7 +74,6 @@ int main (int argc, char *argv[]) {
     memmove(&buffer[3],&filename,filenameLength);
     memmove(&buffer[3+filenameLength],&uifileSize,4);
     
-
     //server structure
     bzero(&dest, sizeof(dest));
     dest.sin_family = AF_INET;
@@ -82,7 +81,7 @@ int main (int argc, char *argv[]) {
     dest.sin_port = htons(atoi(argv[2]));
     //sending header
     usleep(100000);
-    if((sendto(sock,buffer,strlen(buffer)+1,0,(struct sockaddr*) &dest,sizeof(struct sockaddr_in) )) < 0){
+    if((sendto(sock,buffer,7+filenameLength,0,(struct sockaddr*) &dest,sizeof(struct sockaddr_in) )) < 0){
         printf("errno: %d\n\n", errno);
         printf("%d", sock);
         return 0;
@@ -97,16 +96,21 @@ int main (int argc, char *argv[]) {
         if(((unsigned int) fileSize - current) > (BUFFERSIZE - 6)){
             fread(&buffer[5], BUFFERSIZE - 6, 1, fp);
             current += (unsigned int) ftell(fp);
+            if((send = sendto(sock,buffer,BUFFERSIZE,0,(struct sockaddr*) &dest,sizeof(struct sockaddr_in) )) < 0){
+                printf("cannot sendto server%d", times);
+                return 0;
+            }
         }else{
             fread(&buffer[5], (unsigned int) fileSize, 1, fp);
+            if((send = sendto(sock,buffer,(unsigned int) fileSize - current + 6,0,(struct sockaddr*) &dest,sizeof(struct sockaddr_in) )) < 0){
+                printf("cannot sendto server%d", times);
+                return 0;
+            }
         }
         times++;
         usleep(100000);
-        printf("%s", buffer);
-        if((send = sendto(sock,buffer,sizeof(buffer),0,(struct sockaddr*) &dest,sizeof(struct sockaddr_in) )) < 0){
-            printf("cannot sendto server%d", times);
-            return 0;
-        }
+        //printf("%s", buffer);
+
     }
     
     rewind(fp);
@@ -121,7 +125,7 @@ int main (int argc, char *argv[]) {
     memmove(&buffer[0],&SHA1_T,1);
     memmove(&buffer[1],sha1string,20);
     usleep(100000);
-    if((send = sendto(sock,buffer,sizeof(buffer),0,(struct sockaddr*) &dest,sizeof(struct sockaddr_in) )) < 0){
+    if((send = sendto(sock,buffer,21,0,(struct sockaddr*) &dest,sizeof(struct sockaddr_in) )) < 0){
         printf("cannot sendto server");
         return 0;
     }
